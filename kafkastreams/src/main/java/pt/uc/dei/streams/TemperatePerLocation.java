7b864683ae23a1f10b8d392feb6dcad239dea6d4
José Pedro Aguiar <88104767+josePedroAguiar@users.noreply.github.com>
2022-12-07 21:16:45 +0000

    add stream files

---
+++ kafkastreams/src/main/java/pt/uc/dei/streams/TemperatePerLocation.java
@@ -0,0 +1,21 @@
+package pt.uc.dei.streams;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.TimeWindows;
+
+public class TemperatePerLocation {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
